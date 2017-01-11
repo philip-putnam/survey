1cbb9031da68c5db0ed118b3c3ee64ed722acd60
Philip Putnam <putnamp989@gmail.com>
2017-01-11 12:14:53 -0800

    add radio box for food

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
   $("#blank form").submit(function(event){
     var nameInput = $("input#name").val();
     var transportInput = $("select#transport").val();
+    var foodInput = $("input:radio[name=food]:checked").val();
 
     $(".name").text(nameInput);
     $(".transport").text(transportInput);
+    $(".food").text(foodInput);
 
 
     event.preventDefault();
