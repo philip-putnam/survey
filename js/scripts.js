e2d3f6d8f0986f64b7ebf4ef77f80046e3c97d55
Philip Putnam <putnamp989@gmail.com>
2017-01-11 12:24:26 -0800

    add favorite color user input

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,13 @@ $(document).ready(function(){
     var transportInput = $("select#transport").val();
     var foodInput = $("input:radio[name=food]:checked").val();
     var bornInput = $("input#born").val();
+    var colorInput = $("input#color").val();
 
     $(".name").text(nameInput);
     $(".transport").text(transportInput);
     $(".food").text(foodInput);
     $(".born").text(bornInput);
+    $(".color").text(colorInput);
 
 
     event.preventDefault();
