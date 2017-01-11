62697d40c0507056d30e01103ba408431565f726
Philip Putnam <putnamp989@gmail.com>
2017-01-11 12:00:40 -0800

    add transportation method select box

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
   $("#blank form").submit(function(event){
     var nameInput = $("input#name").val();
+    var transportInput = $("select#transport").val();
 
     $(".name").text(nameInput);
+    $(".transport").text(transportInput);
 
 
     event.preventDefault();
