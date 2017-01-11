6d6773a9f54f5b8f70a77ea5db51e0e35703d4c2
Philip Putnam <putnamp989@gmail.com>
2017-01-11 12:20:59 -0800

    add date of birth, date input calendar

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,12 @@ $(document).ready(function(){
     var nameInput = $("input#name").val();
     var transportInput = $("select#transport").val();
     var foodInput = $("input:radio[name=food]:checked").val();
+    var bornInput = $("input#born").val();
 
     $(".name").text(nameInput);
     $(".transport").text(transportInput);
     $(".food").text(foodInput);
+    $(".born").text(bornInput);
 
 
     event.preventDefault();
