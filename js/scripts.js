92149d87a124e5d2ea689a53472a8e5791a244b5
philip putnam <putnamp989@gmail.com>
2017-01-16 11:29:23 -0800

    add loop to jquery in scripts.js, cycling through user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,33 @@
 $(document).ready(function(){
   $("#blank form").submit(function(event){
-    var nameInput = $("input#name").val();
-    var transportInput = $("select#transport").val();
-    var foodInput = $("input:radio[name=food]:checked").val();
-    var bornInput = $("input#born").val();
-    var colorInput = $("input#color").val();
+    var blanks = ["name", "transport", "food", "born", "color"];
 
-    $(".name").text(nameInput);
-    $(".transport").text(transportInput);
-    $(".food").text(foodInput);
-    $(".born").text(bornInput);
-    $(".color").text(colorInput);
+    blanks.forEach(function(blank) {
+      if (blank === "food") {
+        var userInput = $("input:radio[name=" + blank + "]:checked").val();
+      }
+      else if (blank === "transport") {
+        var userInput = $("select#" + blank).val();
+      }
+      else {
+        var userInput = $("input#" + blank).val();
+      }
+      $("." + blank).text(userInput);
+    });
+
+
+
+    // var nameInput = $("input#name").val();
+    // var transportInput = $("select#transport").val();
+    // var foodInput = $("input:radio[name=food]:checked").val();
+    // var bornInput = $("input#born").val();
+    // var colorInput = $("input#color").val();
+    //
+    // $(".name").text(nameInput);
+    // $(".transport").text(transportInput);
+    // $(".food").text(foodInput);
+    // $(".born").text(bornInput);
+    // $(".color").text(colorInput);
 
 
     event.preventDefault();
